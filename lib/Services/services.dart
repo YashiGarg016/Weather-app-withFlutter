4d22c21cde3588062a1847b7dba774c5714c3d4c
Yashi Garg <yashigarg016@gmail.com>
2024-10-14 01:10:00 +0530

    commiting all files

---
+++ lib/Services/services.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/Model/weather_model.dart';
+
+class WeatherServices {
+  fetchWeather() async {
+    final response = await http.get(
+      Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?lat=18.5204&lon=73.8567&appid=dbf9cc68ace330d59920160adeaa680b"),
+    );
+    try {
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+        return WeatherData.fromJson(json);
+      } else {
+        throw Exception('Failed to load Weather data');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
